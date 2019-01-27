ab379a2ede1630ded1922dceb6f3083ed358ba3f
Bo Granlund <bo.granlund@gmail.com>
2019-01-27 14:50:34 +0200

    Tehtava 1.6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const App = () => {
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+	return (
+		<div>
+			<h1>Anna palautetta</h1>
+			<p>
+				<button onClick={() => setGood(good + 1)} >hyvä</button><button onClick={() => setNeutral(neutral + 1)} >neutraali</button><button onClick={() => setBad(bad + 1)} >huono</button>
+			</p>
+			<p>
+				<h1>statistiikka</h1>
+			</p>
+			<p>
+				hyvä {good}
+			</p>
+			<p>
+				neutraalli {neutral}
+			</p>
+			<p>
+				huono {bad}
+			</p>
+
+		</div>
+	)
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
