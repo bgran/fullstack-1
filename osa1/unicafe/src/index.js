22874c1978c6d8eaed58285cdc7649c13dc6d1a5
Bo Granlund <bo.granlund@gmail.com>
2019-01-27 15:30:56 +0200

    Tehtava 1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,42 +1,85 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 
+const Statistics = (props) => {
+	let g = props.data["good"]
+	let n = props.data["neutral"]
+	let b = props.data["bad"]
+
+	console.log("g: " + g)
+	console.log("n: " + n)
+	console.log("b: " + b)
+	// YHTEENSA
+	let yhteensa = g + n + b
+	console.log("Yhteensa: " + yhteensa)
+
+	let laskuri = 0
+	for (let i=0; i < g ; i++) {
+		laskuri += 1
+	}
+	for(let i =0; i<n; i++) {
+		laskuri += 0
+	}
+	for(let i=0; i<b; i++) {
+		laskuri -= 1
+	}
+
+	// KESKIARVO
+	let keskiarvo = 0
+	if  (yhteensa === 0) {
+		keskiarvo = 0
+	} else {
+		keskiarvo = laskuri/yhteensa
+	}
+	console.log("Keskiarvo: " + keskiarvo)
+
+
+	// POSITIIVISIA
+	let posi = 0
+	if  (yhteensa === 0) {
+		posi = 0
+	} else {
+		posi = (g/yhteensa) * 100
+	}
+	console.log("Positiivisia: " + posi)
+	
+	return (
+		<div>
+			<h1>statistiikka</h1>
+		<div>
+			hyvä {g}
+		</div>
+		<div>
+			neutraali {n}
+		</div>
+		<div>
+			huono {b}
+		</div>
+		<div>
+			yhteensä {yhteensa}
+		</div>
+		<div>
+			keskiarvo {keskiarvo}
+		</div>
+		<div>
+			positiivisia {posi} %
+		</div>
+		</div>
+
+	)
+
+}
 
 const App = () => {
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
 
-	let yhteensa = good + neutral + bad
-	console.log("Yhteensa: " + yhteensa)
-
-	let laskuri = 0
-	for(let i =0; i<good; i++) {
-		laskuri += 1
+	const data = {
+		"good": good,
+		"neutral": neutral,
+		"bad": bad
 	}
-	for(let i =0; i<neutral; i++) {
-		laskuri += 0
-
-	}
-	for(let i =0; i<bad; i++) {
-		laskuri -= 1
-
-	}
-	let keskiarvo = 0
-	if  (yhteensa === 0) {
-		keskiarvo = 0
-	} else {
-		keskiarvo = laskuri/yhteensa
-	}
-	console.log("Keskiarvo: "+keskiarvo)
-
-	let posi = 0
-	if  (yhteensa === 0) {
-		posi = 0
-	} else {
-		posi = (good/yhteensa)* 100
-	}
-	console.log("posi " + posi)
 
 
 	return (
@@ -45,29 +88,7 @@ const App = () => {
 			<p>
 				<button onClick={() => setGood(good + 1)} >hyvä</button><button onClick={() => setNeutral(neutral + 1)} >neutraali</button><button onClick={() => setBad(bad + 1)} >huono</button>
 			</p>
-			<p>
-				<h1>statistiikka</h1>
-			</p>
-			<div>
-				hyvä {good}
-			</div>
-			<div>
-				neutraalli {neutral}
-			</div>
-			<div>
-				huono {bad}
-			</div>
-			<div>
-				yhteensä {yhteensa}
-			</div>
-			<div>
-				keskiarvo {keskiarvo}
-			</div>
-			<div>
-				positiivisia {posi} %
-			</div>
-			
-
+				<Statistics data={data} />
 		</div>
 	)
 
