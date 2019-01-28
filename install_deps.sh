fb076bd0f329d47524de8ac458e9015525b4e3f3
Bo Granlund <bo.granlund@gmail.com>
2019-01-28 17:05:43 +0200

    Cleanups

--- install_deps.sh
+++ install_deps.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -x
 cd osa1 || exit 1
 for teht in kurssitiedot unicafe anekdootit; do (
-	npx create-react-app $teht || exit 2
-	cd $teht || exit 3
-	npm install -s react@16.8.0-alpha.0 react-dom@16.8.0-alpha.0 || exit 4
+	npx create-react-app $teht
+	cd $teht
+	npm install -s react@16.8.0-alpha.0 react-dom@16.8.0-alpha.0
 	)
 done
 
