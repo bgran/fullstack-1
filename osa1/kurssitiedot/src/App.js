45069d512046ebbeff8d4fb894efa8069a53fc10
Bo Granlund <bo.granlund@gmail.com>
2019-01-28 15:22:39 +0200

    Tehtavan 1.5 palautus.

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const Header = (props) => {
+	return (
+		<h1>{props.course_data["name"]}</h1>
+	)
+
 }
 
-export default App;
+const Item = (props) => {
+	return (
+		<div>
+			{props.name}: {props.harkkoja}
+		</div>
+	)
+}
+
+const Content = (props) => {
+
+	return (
+		<div>
+			{props.course_data["parts"].map((item, index) => (
+				<Item name={item["name"]} harkkoja={item["harkkoja"]} key={index} />
+			))}
+		</div>
+	)
+}
+
+const Total = (props) => {
+	let total = 0
+	for (let i=0; i<props.course_data["parts"].length; i++) {
+		total += props.course_data["parts"][i]["harkkoja"]
+	}
+	return (
+		<div>
+			{total} harkkoja
+		</div>
+	)
+}
+
+const App = () => {
+	const course_data = {
+		"name": "Half Stack -sovelluskehitys",
+		"parts":[
+			{"name": "Reacting perusteet", "harkkoja":10},
+			{"name": "Tiedonvälitys propseilla", "harkkoja":7},
+			{"name": "Kompoenttien tila", "harkkoja":14}
+		]
+	}
+
+
+	//const part1 = 'Reactin perusteet'
+	//const exercises1 = 10
+	//const part2 = 'Tiedonvälitys propseilla'
+	//const exercises2 = 7
+	//const part3 = 'Komponenttien tila'
+	//const exercises3 = 14
+
+	return (
+		<div>
+			<Header course_data={course_data} />
+			<Content course_data={course_data} />
+			<Total course_data={course_data} />
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
